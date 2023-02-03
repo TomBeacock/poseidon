fb5812465c95190237a32b5d7aa15d3498ed73fa
Thomas Beacock <thomasbeacock@gmail.com>
2023-02-03 21:29:13 +0000

    Basic event loop

--- core/system/application.cpp
+++ core/system/application.cpp
@@ -10,7 +10,16 @@ namespace poseidon
 
 		SDL_Window* window = SDL_CreateWindow("Window Title", 100, 100, 1280, 720, SDL_WINDOW_OPENGL);
 
-		SDL_Delay(3000);
+		bool running = true;
+		while (running)
+		{
+			SDL_Event event;
+			while (SDL_PollEvent(&event))
+			{
+				if (event.type == SDL_QUIT)
+					running = false;
+			}
+		}
 
 		SDL_DestroyWindow(window);
 
