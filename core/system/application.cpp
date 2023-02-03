d69d0afb948274da84af0771fed1eb10fa53d7ad
Thomas Beacock <thomasbeacock@gmail.com>
2023-02-03 21:57:01 +0000

    Loaded OpenGL functions using glad

--- core/system/application.cpp
+++ core/system/application.cpp
@@ -1,6 +1,7 @@
 #include "application.h"
 
 #include <SDL.h>
+#include <glad/gl.h>
 
 namespace poseidon
 {
@@ -8,7 +9,16 @@ namespace poseidon
 	{
 		SDL_Init(SDL_INIT_VIDEO);
 
+		SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 4);
+		SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 1);
+		SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+		SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
+		SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
+
 		SDL_Window* window = SDL_CreateWindow("Window Title", 100, 100, 1280, 720, SDL_WINDOW_OPENGL);
+		SDL_GLContext gl_context = SDL_GL_CreateContext(window);
+
+		gladLoadGL((GLADloadfunc)SDL_GL_GetProcAddress);
 
 		bool running = true;
 		while (running)
@@ -19,6 +29,11 @@ namespace poseidon
 				if (event.type == SDL_QUIT)
 					running = false;
 			}
+
+			glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+			glClear(GL_DEPTH_BUFFER_BIT | GL_COLOR_BUFFER_BIT);
+
+			SDL_GL_SwapWindow(window);
 		}
 
 		SDL_DestroyWindow(window);
