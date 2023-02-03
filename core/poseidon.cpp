512273b044a28ff7b4fcff0c192bc1e12b03527e
Thomas Beacock <thomasbeacock@gmail.com>
2023-02-03 18:09:48 +0000

    Project setup

---
+++ core/poseidon.cpp
@@ -0,0 +1,13 @@
+﻿#include "poseidon.h"
+
+#include <SDL.h>
+#include <iostream>
+
+using namespace std;
+
+void poseidon::init()
+{
+	SDL_Init(SDL_INIT_VIDEO);
+	cout << "Initialized" << endl;
+	SDL_Quit();
+}
---
+++ editor/editor.cpp
@@ -0,0 +1,9 @@
+#include <SDL.h>
+
+#include <poseidon.h>
+
+int main(int argc, char* argv[])
+{
+	poseidon::init();
+	return 0;
+}
