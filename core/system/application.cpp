f768895d71c9d0b3a7f87b30402446adcc604498
Thomas Beacock <thomasbeacock@gmail.com>
2023-02-04 23:33:46 +0000

    Updated to use OpenGL 4.5

--- core/system/application.cpp
+++ core/system/application.cpp
@@ -10,7 +10,7 @@ namespace poseidon
 		SDL_Init(SDL_INIT_VIDEO);
 
 		SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 4);
-		SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 1);
+		SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 5);
 		SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
 		SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
 		SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
@@ -19,7 +19,6 @@ namespace poseidon
 		SDL_GLContext gl_context = SDL_GL_CreateContext(window);
 
 		gladLoadGL((GLADloadfunc)SDL_GL_GetProcAddress);
-
 		bool running = true;
 		while (running)
 		{
