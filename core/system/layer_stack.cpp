3c5b2b9bc7162d433e515f9e56cc930430d57cf2
Thomas Beacock <thomasbeacock@gmail.com>
2023-02-06 12:07:01 +0000

    Added layer architecture

---
+++ core/system/layer_stack.cpp
@@ -0,0 +1,29 @@
+#include "layer_stack.h"
+
+#include "layer.h"
+
+namespace poseidon
+{
+	void LayerStack::onUpdate()
+	{
+		for (auto& layer : layerStack_)
+		{
+			layer->onUpdate();
+		}
+	}
+
+	bool LayerStack::onEvent(const SDL_Event& event)
+	{
+		for (auto& layer : layerStack_)
+		{
+			if (layer->onEvent(event))
+				return true;
+		}
+		return false;
+	}
+
+	void LayerStack::add(std::unique_ptr<Layer> layer)
+	{
+		layerStack_.push_back(std::move(layer));
+	}
+}
