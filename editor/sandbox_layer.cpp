e972c0595478118d7f63e9c21916d4f0099c1feb
Thomas Beacock <thomasbeacock@gmail.com>
2023-02-07 17:14:35 +0000

    Updated sandbox to use renderer 2d

--- editor/sandbox_layer.cpp
+++ editor/sandbox_layer.cpp
@@ -81,74 +81,8 @@ void main() {
 		cubeShader_->setMat4("u_model", model);
 		cubeShader_->setMat4("u_viewProjection", projection * view);
 
-		// Example textured quad
-		// Create vertex buffer
-		float quadVertices[4 * 5] = {
-			-0.5, -0.5, -0.0, 0.0f, 0.0f,
-			0.5, -0.5, -0.0, 1.0f, 0.0f,
-			0.5, 0.5, -0.0, 1.0f, 1.0f,
-			-0.5, 0.5, -0.0, 0.0f, 1.0f
-		};
-		quadVbo_ = std::make_unique<ArrayBuffer>(
-			&quadVertices,
-			sizeof(quadVertices),
-			BufferLayout({ DataType::Vec3, DataType::Vec2 })
-		);
-
-		// Create index buffer
-		std::array<uint32_t, 6> quadIndices = { 0, 1, 2, 0, 2, 3 };
-		quadIbo_ = std::make_unique<IndexBuffer>(quadIndices.data(), quadIndices.size());
-
-		// Create vertex array
-		quadVao_ = std::make_unique<VertexArray>();
-		quadVao_->addVertexBuffer(*quadVbo_);
-		quadVao_->setIndexBuffer(*quadIbo_);
-
-		// Create shader
-		const char* quadVertexShader = R"(#version 330 core
-layout(location = 0) in vec3 v_in_pos;
-layout(location = 1) in vec2 v_in_uv;
-
-out vec2 v_out_uv;
-
-uniform mat4 u_model;
-uniform mat4 u_viewProjection;
-
-void main() {
-	v_out_uv = v_in_uv;
-	gl_Position = u_viewProjection * u_model * vec4(v_in_pos, 1.0);
-})";
-
-		const char* quadFragmentShader = R"(#version 330 core
-in vec2 v_out_uv;
-
-out vec4 f_out_color;
-
-uniform sampler2D u_texture;
-
-void main() {
-	f_out_color = texture(u_texture, v_out_uv);
-})";
-
-		quadShader_ = std::make_unique<Shader>(quadVertexShader, quadFragmentShader);
-		quadShader_->bind();
-
 		// Load texture
 		quadTexture_ = std::make_unique<Texture>(RES_DIR "trident.png");
-		quadTexture_->slot(0);
-
-		// Set uniforms
-		Mat4 quadModel = Mat4::transformation(
-			{ 0.0f, 0.0f, 0.0f },
-			{ 0.0f, 0.0f, 0.0f },
-			{ 1.0f, 1.0f, 1.0f }
-		);
-		Mat4 quadView = Mat4::translation(-Vec3(0.0f, 0.0f, -5.0f));
-		Mat4 quadProjection = Mat4::ortho(16.0f, 9.0f, 0.1f, 10.0f);
-
-		quadShader_->setMat4("u_model", quadModel);
-		quadShader_->setMat4("u_viewProjection", quadProjection * quadView);
-		quadShader_->setInt("u_texture", 0);
 
 		Renderer::setClearColor({ 0.0f, 0.0f, 0.0f, 1.0f });
 	}
@@ -167,8 +101,8 @@ void main() {
 		Renderer::drawIndexed(*cubeVao_, 6 * 6);
 
 		// Render textured quad
-		quadShader_->bind();
-		Renderer::drawIndexed(*quadVao_, 6);
+		Renderer2D::drawRect(Bounds(Vec2::zero, { 2.0f, 10.0f }), { 0.5f, 0.5f, 0.0f, 1.0f });
+		Renderer2D::drawRect(Bounds(Vec2::zero, { 5.0f, 5.0f }, { 0.5f, 0.5f }), Bounds(Vec2::zero, Vec2::one), *quadTexture_, Vec4::one);
 	}
 
 	bool SandboxLayer::onEvent(const SDL_Event& event)
